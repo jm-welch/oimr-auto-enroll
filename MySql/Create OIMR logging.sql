50a4f6416d189d153821d00059eaeba61cbbf1a8
Toar <toarthos@gmail.com>
2020-09-18 13:55:36 -0500

    changed comments

--- MySql/Create OIMR logging.sql
+++ MySql/Create OIMR logging.sql
@@ -1,8 +1,9 @@
 -- table to hold logged events for regfox<->google classrooom Bridge
+drop table oimr_logging;
+
 create table oimr_logging
 (
-    recno     int          not null
-        primary key,
+    recno     int          NOT NULL AUTO_INCREMENT,
     log_level varchar(24)  null,
     module    varchar(50)  null,
     method    varchar(50)  null,
--- oimrConnect.py
+++ oimrConnect.py
@@ -1,32 +1,58 @@
+#!/usr/bin/python3.7
+"""
+OIMR: brige project:
+author Toar Schell
+
+what id does:
+1) creates a ssh tunnel for remoting into PythonAnywhere.
+2) Use that tunnel to aquire a locol remote port for a SqlAlchemy connection string.
+3) create multiple table hooks for use of Odo which will do bulk inserts rapidly.
+
+requirements: pythonAnywhere_secret json file that holds passwords and usernames.
+Modules: sqlAlchemy, pandas odo multipledispatch
+globals:
+Notes as of 2020:
+sshtunnel (requires visual studio build tools so you may have to go to https://visualstudio.microsoft.com/downloads/
+PythonAnywhere is uptodate and has installed nicely,but if using locally you may need the c++ build tools.
+odo - be careful here the pip install will an older version that does not implement pandas subroutines correctly.
+https://github.com/blaze/odo has a version that does so the steps are pip install odo then copy and over-write the odo directory in site-site packages.
+as of 09152020 this has been done on the pythonanywhere/user/home/.local/python37 implementation.
+so make sure all of your scripts use a #!/usr/bin/python3.7 when running inside pythonAnywhere.
+odo requires an input into mysql tables thusly with comma delimited records inside brackets:
+[
+[None, "Debug", "oimrConnect", "make_db_connect", 12, todaysDate,"All work and no play makes Jeremy a Dull boy"],
+[None, "Info", "registration", "make_whoopie", 666, todaysDate,'all Play and no Work makes Jeremy a dull boy'],
+[None, "debug", "oimrConnect", "print_all", 34, todaysDate, "I guess Jeremy is dull"]
+]
+one gotcha for mysql autoincrementing columns is that it requries a value in the identity field.
+yours truly found a way around this by using a None object to pass to the database and Lo it Worked.
+#TODO: post this solution to pythonAnywhere as well as stackoverflow given that its an undocumented solution.
+"""
 import sqlalchemy as sa
 import sshtunnel
-# import pandas as pd
-# from os import getenv
-import json
-import logging
 import time
+import json
 import collections
-# import odo
-import registration_mysql as reg
+import odo
 
 sshtunnel.SSH_TIMEOUT = 5.0
 sshtunnel.TUNNEL_TIMEOUT = 5.0
-api, registrants = reg.makeRegistrationList('regfox_secret.json')
 
 
 def multi_dimensions(n, type):
-    """ Creates an n-dimension dictionary where the n-th dimension is of type 'type'
+    """
+    Creates an n-dimension dictionary where the n-th dimension is of type 'type'
     """
     if n <= 1:
         return type()
     return collections.defaultdict(lambda: multi_dimensions(n - 1, type))
 
-
 collections.Counter()
 
 
 class PyAnywhereAPI():
-    def __init__(self, inputFile=None, tunnel=None, arrConn=None):
+
+    def __init__(self, inputFile=None):
         """ If an inputFile is passed, use that,
             otherwise, look for explicit values """
 
@@ -39,112 +65,136 @@ class PyAnywhereAPI():
                 self.userPass = pyAInfo['userPass']
                 self.dbPass = pyAInfo['dbPass']
                 self.dbName = pyAInfo['dbName']
-                # arrConn = self.make_arrConnParams()
-                # tunnel = self.make_tunnel(arrConn)
+        # lets expose global class level objects
+        self.arrConn = multi_dimensions(5, collections.Counter)
+        self.gPaTunnel = None
+        self.gPaEngine = None
+        self.gMysqlConn = None
+        # ok lets make connection array and and use sshTunnel forwarding to grab a port for sqlAlchemy engine
+        self.make_arrConnParams()
+        # Make the ssh tunnel into pythonAnywhere
+        self.make_tunnel()
+        # now lets use tunnel to create a connection string then
+        # create the SqlAlchemy Engine with tunnel,
+        # this will create/begin a SqlAlchemy engine, start a connection to oimr$OIMR database
+        # and finally expose the mysql log table so that odo can write to it.
+        self.make_connection()
 
     def make_arrConnParams(self):
         # This is multi dimensional so we can use it for other connections to any other databases we may want to set up.
-        arrConnParams = multi_dimensions(5, collections.Counter)
-        arrConnParams['pa']['OIMR']['userName'] = self.userName
-        arrConnParams['pa']['OIMR']['userPass'] = self.userPass
-        arrConnParams['pa']['OIMR']['dbPass'] = self.dbPass
-        arrConnParams['pa']['OIMR']['dbName'] = self.dbName
-        arrConnParams['pa']['OIMR']['ssh'] = 'ssh.pythonanywhere.com'
-        arrConnParams['pa']['OIMR']['dbUrl'] = 'oimr.mysql.pythonanywhere-services.com'
-        arrConnParams['pa']['OIMR']['dbPort'] = 3306
-        arrConnParams['pa']['OIMR']['host'] = '127.0.0.1'
-        arrConnParams['pa']['OIMR']['localBind'] = 'dynamic after tunnel start'
-        arrConnParams['pa']['OIMR']['connection'] = r'mysql+mysqldb://'
-        return arrConnParams
 
-    def make_tunnel(self, arrConn) -> object:
-        tunnel = sshtunnel.SSHTunnelForwarder(
-            (arrConn['pa']['OIMR']['ssh'])
-            , ssh_username=arrConn['pa']['OIMR']['userName']
-            , ssh_password=arrConn['pa']['OIMR']['userPass']
-            , remote_bind_address=(arrConn['pa']['OIMR']['dbUrl'], arrConn['pa']['OIMR']['dbPort'])
+        self.arrConn['pa']['OIMR']['userName'] = self.userName
+        self.arrConn['pa']['OIMR']['userPass'] = self.userPass
+        self.arrConn['pa']['OIMR']['dbPass'] = self.dbPass
+        self.arrConn['pa']['OIMR']['dbName'] = self.dbName
+        self.arrConn['pa']['OIMR']['ssh'] = 'ssh.pythonanywhere.com'
+        self.arrConn['pa']['OIMR']['dbUrl'] = 'oimr.mysql.pythonanywhere-services.com'
+        self.arrConn['pa']['OIMR']['dbPort'] = 3306
+        self.arrConn['pa']['OIMR']['host'] = '127.0.0.1'
+        self.arrConn['pa']['OIMR']['localBind'] = 'dynamic after tunnel start'
+        self.arrConn['pa']['OIMR']['connection'] = r'mysql+mysqldb://'
+
+    def make_tunnel(self) -> object:
+        self.gPaTunnel = sshtunnel.SSHTunnelForwarder(
+            (self.arrConn['pa']['OIMR']['ssh'])
+            , ssh_username=self.arrConn['pa']['OIMR']['userName']
+            , ssh_password=self.arrConn['pa']['OIMR']['userPass']
+            , remote_bind_address=(self.arrConn['pa']['OIMR']['dbUrl']
+                                   , self.arrConn['pa']['OIMR']['dbPort'])
         )
-        tunnel.start()
+        self.gPaTunnel.start()
         TODO: "MAKE A START/STOP TUNNEL INTERFACE TO CLEAN THINGS UP"
-        return tunnel
 
-    def make_connection_str(self, arrConn, tunnel):
+    def make_connection_str(self):
         # now we capture the tunnel local bind port and use with sqlalchemy mysql dialect and the local port to create a connection string
-        arrConn['pa']['OIMR']['localBind'] = str(tunnel.local_bind_port)
-        arrConn['pa']['OIMR']['connection'] = \
-            arrConn['pa']['OIMR']['connection'] + \
-            arrConn['pa']['OIMR']['userName'] + ":" + \
-            arrConn['pa']['OIMR']['dbPass'] + "@" + \
-            arrConn['pa']['OIMR']['host'] + ":" + \
-            arrConn['pa']['OIMR']['localBind'] + "/" + \
-            arrConn['pa']['OIMR']['dbName']
+        self.arrConn['pa']['OIMR']['localBind'] = str(self.gPaTunnel.local_bind_port)
+        # ok have all the pieces to make a sqlAlchemy acceptable connection string
+        self.arrConn['pa']['OIMR']['connection'] = \
+            self.arrConn['pa']['OIMR']['connection'] + \
+            self.arrConn['pa']['OIMR']['userName'] + ":" + \
+            self.arrConn['pa']['OIMR']['dbPass'] + "@" + \
+            self.arrConn['pa']['OIMR']['host'] + ":" + \
+            self.arrConn['pa']['OIMR']['localBind'] + "/" + \
+            self.arrConn['pa']['OIMR']['dbName']
 
-        return arrConn['pa']['OIMR']['connection']
+    def make_connection(self):
+        # call make oimr$OIMR database connection string to correctly populate value of self.arrConn['pa']['OIMR']['connection']
+        self.make_connection_str()
+        # use make_connection_str results to fire up a sqlAlchemy connection into PythonAnywhere using mysql dialect
+        self.gPaEngine = sa.create_engine(self.arrConn['pa']['OIMR']['connection'], pool_recycle=280)
+        # TODO: MAKE A CONNECTION POOL HERE FOR VAROUS HOOKS INTO ENGINE
+        test = self.gPaEngine.get_execution_options()
+        self.gPaMysqlConn = self.gPaEngine.connect()
+        self.gPaMysqlConn.begin()
 
-    def close_connection(self, mysqlConn, tunnel):
-        if not mysqlConn.closed:
-            mysqlConn.close()
-            print('Connection closed')
+        metadata = sa.MetaData(bind=self.gPaMysqlConn)
+        self.mysqlLogTable = sa.Table('oimr_logging', metadata,
+                                      sa.Column('recno', sa.Integer, primary_key=True),
+                                      sa.Column('log_level', sa.VARCHAR),
+                                      sa.Column('module', sa.VARCHAR, primary_key=True),
+                                      sa.Column('method', sa.VARCHAR, primary_key=True),
+                                      sa.Column('line_num', sa.Integer, primary_key=True),
+                                      sa.Column('mess_date', sa.DATETIME),
+                                      sa.Column('message', sa.VARCHAR)
+                                      )
+        self.make_log_entry("Info", "oimrConnect", "make_connection",
+                            "Established connection to Mysql database log table")
+
+        # return conn, eng, tblOimrLogging
+        def close_connection(self, tun, eng, sqlConn):
+            # TODO: SEE HOW THIS HOOKS IN WITH A SESSION WHICH SEEMS TO BE THE PROPER WAY TO CLOSE A SQLALCHEMY CONN.
+            if sqlConn:
+                if not self.gPaMysqlConn.closed:
+                    self.gPaMysqlConn.close()
+                    print('Connection closed')
+            if eng:
+                self.gPaEngine.dispose()
+            if tun:
+                if self.gPaTunnel.is_active:
+                    print("check tunnel here")
 
     def close_tunnel(self, tunnel):
         if tunnel.is_active:
+            tunnel.is_active = False
+
             # TODO: write close script
             # TODO: write error code
             print("should we close tunnel?")
 
-    def make_connection(self, arrConn, tunnel):
-        # connString = self.make_connection_str(arrConn,tunnel)
-        # use make_connection_str to getnerate the correct url for a sqlalchemy connection into PythonAnywhere
-        eng = sa.create_engine(self.make_connection_str(arrConn, tunnel), pool_recycle=280)
-        test = eng.get_execution_options()
-        conn = eng.connect()
-        conn.begin()
-
-        metadata = sa.MetaData(bind=conn)
-        tblOimrLogging = sa.Table('oimr_logging', metadata,
-                                  sa.Column('recno', sa.Integer, primary_key=True),
-                                  sa.Column('log_level', sa.VARCHAR),
-                                  sa.Column('module', sa.VARCHAR, primary_key=True),
-                                  sa.Column('method', sa.VARCHAR, primary_key=True),
-                                  sa.Column('line_num', sa.Integer, primary_key=True),
-                                  sa.Column('mess_date', sa.DATETIME),
-                                  sa.Column('message', sa.VARCHAR)
-                                  )
-
-        return conn, eng, tblOimrLogging
-
-
-def makeLogFile(mess):
-    todaysDate = (time.strftime("%d_%m_%Y"))
-    pthLog = r".\logs\oimr_"
-    logFileName = pthLog
-    logFileNameExt = ".log"
-    finalFileName = logFileName + todaysDate + logFileNameExt
-    # \\cityofmesquite.com\Files\GIS\GISDS\projects\scheduled\Replication\logs\
-    logging.basicConfig(filename=finalFileName, format='%(levelname)s - %(asctime)s: %(message)s',
-                        datefmt='%m/%d/%Y %I:%M:%S %p', filemode='a', level=logging.DEBUG)
-
-
-def makeOimrConnection(secretJson, **kwargs):
-    # make api to create connections strings for ssh tunnel into pythonAnywhere and its mysql database
-    pyAnyApi = PyAnywhereAPI(secretJson)
-    # create parameter object that contains all strings and values to create connection strings
-    arrConn = pyAnyApi.make_arrConnParams()
-    # Make the ssh tunnel into python anywhere
-    tunnel = pyAnyApi.make_tunnel(arrConn)
-    # create the SqlAlchemy Engine with tunnel, and use it to make a mysal connection to get access to log table
-    mysqlConn, engine, mysqlLogTable = (pyAnyApi.make_connection(arrConn, tunnel))
-    # retun all the elements
-    return pyAnyApi, arrConn, tunnel, mysqlConn, engine, mysqlLog
+    def make_log_entry(self, log_level, module, method, message, err_line=0):
+        timeStampNow = (time.strftime("%Y-%m-%d %I:%M:%S"))
+        recno = None  # recno is the id field and autoincrements. odo can use a None object to
+        log_string = [[recno, log_level, module, method, err_line, timeStampNow, message]]
+        logDs = odo.discover(self.mysqlLogTable)
+        odo.odo(log_string, self.mysqlLogTable, dshape=logDs, bind=self.gPaEngine, raise_on_errors=True)
 
 
 if __name__ == '__main__':
-    pyAnyApi, arrConn, tunnel, mysqlConn, engine, mysqlLog = makeOimrConnection('PythonAnywhere_secret.json')
-    logging.basicConfig()
+    pyAnyApi = PyAnywhereAPI('PythonAnywhere_secret.json')
+    print('done')
+    """
+    pyAnyApi, arrConn, gPaTunnel, gPaMysqlConn, gPaEngine, mysqlLogTable = makeOimrConnection(
+        'PythonAnywhere_secret.json')
+    arrConn = pyAnyApi.make_arrConnParams()
+    gPaTunnel = pyAnyApi.make_tunnel(arrConn)
+    # suppose the database has been restarted.
 
-    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-
-    mysqlClose = (pyAnyApi.close_connection(mysqlConn, engine, tunnel))
+    timeStampNow = (time.strftime("%Y-%m-%d %I:%M:%S"))
+    logDs = odo.discover(pyAnyApi.mysqlLogTable)
+    errLog = [
+        [None, "Debug", "oimrConnect", "make_db_connect", 12, timeStampNow,"All work and no play makes Jeremy a Dull boy"],
+        [None, "Info", "registration", "make_whoopie", 666, timeStampNow,'all Play and no Work makes Jeremy a dull boy'],
+        [None, "debug", "oimrConnect", "print_all", 34, timeStampNow, "I guess Jeremy is dull"]
+    ]
+    print(errLog)
+    errLog = odo.odo(errLog, pyAnyApi.mysqlLogTable, dshape=logDs, bind=pyAnyApi.gPaEngine, raise_on_errors=True)
+    print(errLog)
+    closeTunnel = True
+    closeEngine = True
+    closeMysqlconn = True
+    mysqlClose = pyAnyApi.close_connection(closeMysqlconn, closeEngine, closeTunnel)
+    
+    """
 # registrants.print_report()
 
 
