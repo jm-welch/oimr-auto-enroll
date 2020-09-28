01fb692ab1dc503cabaa49337c298e67f0b4753d
Jeremy Welch <jm-welch@users.noreply.github.com>
2020-09-28 14:41:52 +0000

    Add error

--- enrollment.py
+++ enrollment.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import logging
